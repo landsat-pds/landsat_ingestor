a437a7aa8eccfe5243e2f09c785f92989659fc3e
Frank Warmerdam <warmerdam@pobox.com>
2015-03-01 22:35:53 -0800

    improve error handling without access keys

--- ingestor/l8_aws_config.py
+++ ingestor/l8_aws_config.py
@@ -2,6 +2,12 @@ import os
 
 BUCKET_NAME = 'landsat-pds'
 BUCKET_URL = 'https://s3-us-west-2.amazonaws.com/landsat-pds'
-ACCESS_ID = os.environ['LANDSAT_PDS_ACCESS_ID']
-ACCESS_KEY = os.environ['LANDSAT_PDS_ACCESS_KEY']
 
+ACCESS_ID = os.environ.get('LANDSAT_PDS_ACCESS_ID',None)
+ACCESS_KEY = os.environ.get('LANDSAT_PDS_ACCESS_KEY',None)
+
+if ACCESS_ID is None or ACCESS_KEY is None:
+    print
+    print 'WARNING: Missing one or both of LANDSAT_PDS_ACCESS_ID and '
+    print '         LANDSAT_PDS_ACCESS_KEY environment variables.'
+    print
