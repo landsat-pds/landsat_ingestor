7d2ec009405dda6d7724aa8ff0ac6a9545462cef
Amit Kapadia <amit@planet.com>
2015-04-13 10:48:20 -0700

    return when corrupt file attempts to process

--- ingestor/l8_process_scene.py
+++ ingestor/l8_process_scene.py
@@ -60,6 +60,7 @@ def process(source, scene_root, verbose=False, clean=False, list_file=None,
     except:
         if source is 's3queue':
             puller_s3queue.move_to_corrupt_queue(scene_root)
+            return
 
     scene_info.add_mtl_info(scene_dict, scene_root, local_dir)
     
